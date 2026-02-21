97988cbc89a159a923eedff6efcc62e60a03e061
khe4oyan <khe4oyan@gmail.com>
2026-02-21 11:22:32 +0400

    [feat] purchase status change controllers

--- src/controllers/purchase/approvedPurchase.controller.js
+++ src/controllers/purchase/approvedPurchase.controller.js
@@ -1,4 +1,23 @@
+// services
+import getPurchaseByIdService from '../../services/purchase/getPurchaseById.service.js';
+import purchaseStatusChangeService from '../../services/purchase/purchaseStatusChange.service.js';
+
+// utils
+import AppError from '../../utils/AppError.class.js';
+
+// constants
+import { PURCHASE_STATUSES } from '../../constants/purchaseStatusTypes.js';
+
 export default async function approvedPurchase(req, res) {
-  // TODO
-  // if status is submit then can change
+  const purchaseId = +req.params.id;
+  const purchaseData = await getPurchaseByIdService(purchaseId);
+
+  // check purchase status
+  if (purchaseData.status !== PURCHASE_STATUSES.SUBMITTED) {
+    throw new AppError("Only submited purchases can be approved", 400);
+  }
+
+  await purchaseStatusChangeService(purchaseId, PURCHASE_STATUSES.APPROVED);
+
+  res.status(200).json({ success: true });
 }
--- src/controllers/purchase/rejectedPurchase.controller.js
+++ src/controllers/purchase/rejectedPurchase.controller.js
@@ -1,4 +1,23 @@
+// services
+import getPurchaseByIdService from '../../services/purchase/getPurchaseById.service.js';
+import purchaseStatusChangeService from '../../services/purchase/purchaseStatusChange.service.js';
+
+// utils
+import AppError from '../../utils/AppError.class.js';
+
+// constants
+import { PURCHASE_STATUSES } from '../../constants/purchaseStatusTypes.js';
+
 export default async function rejectedPurchase(req, res) {
-  // TODO
-  // if status is submit then can change
+  const purchaseId = +req.params.id;
+  const purchaseData = await getPurchaseByIdService(purchaseId);
+
+  // check purchase status
+  if (purchaseData.status !== PURCHASE_STATUSES.SUBMITTED) {
+    throw new AppError("Only submited purchases can be rejected", 400);
+  }
+
+  await purchaseStatusChangeService(purchaseId, PURCHASE_STATUSES.REJECTED);
+
+  res.status(200).json({ success: true });
 }
