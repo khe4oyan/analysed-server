772466e27a8dbe9be492de83f2be2102d54d5eb2
khe4oyan <khe4oyan@gmail.com>
2026-02-21 10:44:49 +0400

    [feat] added condition

--- src/controllers/purchase/editPurchase.controller.js
+++ src/controllers/purchase/editPurchase.controller.js
@@ -5,10 +5,18 @@ import editPurchaseByIdService from '../../services/purchase/editPurchaseById.se
 // utils
 import AppError from '../../utils/AppError.class.js';
 
+// constants
+import { PURCHASE_STATUSES } from '../../constants/purchaseStatusTypes.js';
+
 export default async function editPurchase(req, res) {
   // get purchase
   const purchaseId = +req.params.id;
   const purchaseData = await getPurchaseByIdService(purchaseId);
+
+  // check purchase status
+  if (purchaseData.status !== PURCHASE_STATUSES.DRAFT) {
+    throw new AppError("You cant change it. Purchase is submitted/approved or rejected.", 403);
+  }
   
   // check created_by_id
   const userData = req.userData;
